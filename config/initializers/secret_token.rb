773d0c5d91e04f6e93a7b8d9c39a2f583e47bd61
Wale <adewale.desalu@gmail.com>
2013-09-14 16:54:35 -0400

    start of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Healthapp::Application.config.secret_key_base = '07d1f53bedd5adbed0f1857edf82455222e8b0e9f0989e8834d639c3c437ec57306dcfb01c8080aca5877aa97ffb838272390826422b7e6daeb4dfe718810aad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Healthapp::Application.config.session_store :cookie_store, key: '_healthapp_session'
